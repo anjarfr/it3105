2b33757d46b9721f7cd7e2e8d9f2fb1b0df243c2
Anja <anjarfrom@gmail.com>
2020-01-23 11:48:57 +0100

    Initialize game

--- board.py
+++ board.py
@@ -1,47 +1,50 @@
 import numpy as np
+import yaml
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 class Board:
 
-    def __init__(self, size=3):
+    def __init__(self, shape, size=3):
         self.size = size
-        self.board = self.create_board()
+        self.shape = shape
+        self.board = self.create_board(shape)
 
-    def create_board(self):
-        return None
+    def create_board(self, shape):
+        if self.shape == 'triangle':
+            return self.create_triangle(self.size)
+        if self.shape == 'diamond':
+            return self.create_diamond(self.size)
 
-
-class Triangle(Board):
-    
-    def __init__(self, size):
-        super(Triangle, self).__init__(size)
-
-    def create_board(self):
-        board = np.empty((self.size, self.size), dtype='object')
+    def create_triangle(self, size):
+        board = np.empty((size, size), dtype='object')
         iterator = 1
-        while iterator != self.size + 1:
+        while iterator != size + 1:
             for row in board:
                 for i in range(iterator):
-                    row[i] = Cell(0)
+                    row[i] = Cell((0,0))
                 iterator += 1
         return board
 
-
-class Diamond(Board):
-
-    def __init__(self, size):
-        super(Diamond, self).__init__(size)
-
-    def create_board(self):
-        board = np.empty((self.size, self.size), dtype='object')
+    def create_diamond(self, size):
+        board = np.empty((size, size), dtype='object')
         for row in board:
             for i in range(row.size):
-                row[i] = Cell(0)
+                row[i] = Cell((0,0))
         return board
+
+    def set_cell(self, r, c, state):
+        try:
+            self.board[r][c].set_state(state)
+        except:
+            pass
 
 
 class Cell:
     """
-    
+    Represents a cell in the board. Has a state that can have
+    3 values: (0,0) - emtpy, (0,1) - player 1, (1,0) - player 2
     """
     
     def __init__(self, state):  
@@ -55,7 +58,5 @@ class Cell:
         self.state = state
 
 
-t = Triangle(5)
-#print(t.board)
-d = Diamond(5)
-print(d.board)
+#b = Board(cfg['board']['shape'], cfg['board']['size'])
+#print(b.board)
--- game.py
+++ game.py
@@ -1,7 +1,32 @@
+import yaml
+from board import Board
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
 class Game:
     pass
 
 class Peg(Game):
      
-     def __init__(self, k=1):
-         self.k = k
+    def __init__(self, open_positions: list, k: int=1):
+        self.k = k
+        self.board = self.init_board(open_positions)
+
+    def init_board(self, open_positions: list):
+        board = Board(cfg['board']['shape'], cfg['board']['size'])
+        for r in range(board.size):
+            for c in range(board.size):
+                coordinate = (r, c)
+                if coordinate in open_positions:
+                    continue
+
+                board.set_cell(r, c, (0,1))
+        return board
+        
+    def place_pieces(self, board: object):
+        pass
+
+
+p = Peg(cfg['game']['open_positions'], cfg['game']['open_cells'])
+print(p.board.board)
