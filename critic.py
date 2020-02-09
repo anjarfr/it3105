2e9218ad9ac26167aae4ab47b4d9f4aa8a9dc3fa
Anja <anjarfrom@gmail.com>
2020-02-09 13:10:37 +0100

    Add Actor and Critic files

---
+++ critic.py
@@ -0,0 +1,9 @@
+class Critic:
+
+    def __init__(self):
+        pass
+    
+    def get_state(self, state):
+        self.state = state
+
+    
--- play.py
+++ play.py
@@ -17,17 +17,7 @@ def main():
 
     visualizer = Visualizer(game.board, game.size, game.shape, display_options)
     visualizer.fill_nodes(game.board.get_filled_cells())
-
-    game.perform_action((0, 3), (1, 2), (2, 1))
-    visualizer.fill_nodes(game.board.get_filled_cells())
-    game.perform_action((3, 0), (2, 1), (1, 2))
-    visualizer.fill_nodes(game.board.get_filled_cells())
-
-    for a in game.history:
-        print(a.get_filled_cells())
-
-    print(game.board.get_filled_cells())
-
+    
 
 if __name__ == "__main__":
     main()
