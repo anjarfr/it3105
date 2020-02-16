5b149d725e8b1683e27f5913e9a74cc593573592
Sindre Bakke Øyen <anjarfrom@gmail.com>
2020-02-16 18:59:15 +0100

    Update NN

--- NN.py
+++ NN.py
@@ -2,60 +2,50 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Model(nn.Module):
-    def __init__(self,nn_dim,input_dim):
-        super(Model,self).__init__()
-        self.input_dim=input_dim
-        self.nn_dim=list(nn_dim)
+
+class NN_Model(nn.Module):
+    def __init__(self, nn_dim, input_dim):
+        super(NN_Model, self).__init__()
+        self.input_dim = input_dim[0]
+        self.nn_dim = nn_dim
 
     def init_network(self):
-        self.hidden=[]
-        for l in range(len(self.nn_dim)): #set layers based on specified dimensions
-            if(l==len(self.nn_dim)-1): #if last layer
-                self.last_l=nn.Linear(self.nn_dim[l-1],1) #only one feature out
-            elif(l==0): #first layer
-                self.first_l=nn.Linear(self.input_dim,self.nn_dim[l])
+        self.hidden = []
+        for l in range(len(self.nn_dim)):  # set layers based on specified dimensions
+            if l == len(self.nn_dim) - 1:  # if last layer
+                self.last_l = nn.Linear(self.nn_dim[l - 1], 1)  # only one feature out
+            elif l == 0:  # first layer
+                print(self.input_dim, self.nn_dim[l])
+                self.first_l = nn.Linear(self.input_dim, self.nn_dim[l])
             else:
-                self.hidden.append(nn.Linear(self.nn_dim[l-1],self.nn_dim[l]))
+                self.hidden.append(nn.Linear(self.nn_dim[l - 1], self.nn_dim[l]))
 
-    def forward(self, state): #propagate the state through the layers and produce an output value
-        input=[]
+    def forward(self, state):  # propagate the state through the layers and produce an output value
+        input = []
         for num in state:
-            input.append(float(num)) #from state as string to state as list
-        input=torch.as_tensor(input) #convert array to tensor
-        output = F.relu(self.first_l(input)) #through first layer
+            input.append(float(num))  # from state as string to state as list
+        input = torch.as_tensor(input)  # convert array to tensor
+        output = F.relu(self.first_l(input))  # through first layer
         for layer in self.hidden:
-            output=F.relu(layer(output))
+            output = F.relu(layer(output))
         value = self.last_l(output)
 
         return value
 
-    #the optimization function stochastic gradient descent
-    def optimfunc(self,lr):
-        optimizer=torch.optim.SGD(self.parameters(),lr)
+    # the optimization function stochastic gradient descent
+    def optimfunc(self, lr):
+        optimizer = torch.optim.SGD(self.parameters(), lr)
         return optimizer.step()
 
-    def lossfunc(self,output,target): #compute the MSE loss
-        criterion=nn.MSELoss()
+    def lossfunc(self, output, target):  # compute the MSE loss
+        criterion = nn.MSELoss()
+        return criterion(output, target)
 
-        return criterion(output,target)
+    def update_func(self, param, e, td_err, learning_rate, grad):  # updates the parameters
+        e = e + grad
+        return param + learning_rate * td_err * e
 
-
-    def update_func(self,param,e,td_err, learning_rate,grad): #updates the parameters
-        e=e+grad
-        return param + learning_rate*td_err*e
-    def gradient(self,e,td_err,lr): #modify gradients
+    def gradient(self, e, td_err, lr):  # modify gradients
         with torch.no_grad():
             for p in self.parameters():
-
                 p.grad = self.update_func(p, e, td_err, lr, p.grad)
-
-
-
-
-
-
-
-
-
-
--- agent/critic.py
+++ agent/critic.py
@@ -3,6 +3,7 @@ from tensorflow.keras import Model, optimizers
 import numpy as np
 from agent.splitgd import SplitGD
 import tensorflow as tf
+from NN import *
 
 
 class Critic:
@@ -83,10 +84,15 @@ class CriticNN(Critic, SplitGD):
 
     def __init__(self, cfg, init_state):
         self.dimensions = cfg["critic"]["dimensions"]  # List
-        model = self.build_model(init_state)
+        # model = self.build_model(init_state)
         Critic.__init__(self, cfg)
-        SplitGD.__init__(self, model)
+        # SplitGD.__init__(self, model)
 
+
+        state = self.generate_state(init_state)
+
+        self.model = NN_Model(self.dimensions, state.shape)
+        self.model.init_network()
         self.eligibility = {}
 
     def generate_state(self, state):
@@ -94,7 +100,7 @@ class CriticNN(Critic, SplitGD):
         listed_state = []
         for c in state:
             listed_state.append(int(c))
-        array = np.array(listed_state)
+        array = np.array(listed_state, dtype=float)
         return array
 
     def build_model(self, init_state):
@@ -121,13 +127,13 @@ class CriticNN(Critic, SplitGD):
         """  """
 
         state = self.generate_state(state)
-        state = np.expand_dims(state, axis=0)
+        #state = np.expand_dims(state, axis=0)
 
         succ_state = self.generate_state(succ_state)
-        succ_state = np.expand_dims(succ_state, axis=0)
+        #succ_state = np.expand_dims(succ_state, axis=0)
 
-        value_function_state = self.model.predict(state)[0, 0]
-        value_function_succ_state = self.model.predict(succ_state)[0, 0]
+        value_function_state = self.model.forward(state)
+        value_function_succ_state = self.model.forward(succ_state)
         print('State value: ', value_function_state, 'Success state value: ', value_function_succ_state)
 
         TD_error = max(-1,
@@ -140,11 +146,13 @@ class CriticNN(Critic, SplitGD):
         print('TD error', TD_error)
         return TD_error
 
+    """
     def reset_eligibility(self):
         for i in range(len(self.model.trainable_weights)):
             weights = self.model.trainable_weights[i].numpy()
             self.eligibility[i] = tf.zeros_like(weights)
-
+    """
+    """
     def modify_gradients(self, gradients, TD_error):
         for i in range(len(gradients)):
             gradient = gradients[i].numpy()
@@ -152,7 +160,22 @@ class CriticNN(Critic, SplitGD):
                 self.eligibility[i].assign(tf.add(self.eligibility[i][j], gradient[0]))
                 gradients[i][0] = gradients[i][0] + self.learning_rate * TD_error[0] * self.eligibility[i][j]
         return gradients
+    """
 
+    def reset_eligibility(self, state):
+        for
+            self.eligibility[state] = 0
+
+    def update_value_function(self, state, TD_error, reward, succ_state):
+        output = self.model.forward(state)
+        target = reward + self.discount_factor * self.model.forward(succ_state)
+        loss = self.model.lossfunc(output, target)
+
+        loss.backward()
+        self.model.gradient(self.eligibility[state], TD_error, self.learning_rate)
+        self.model.optimfunc(self.learning_rate)
+
+    """
     def update_value_function(self, state, TD_error, reward, succ_state):
         state = self.generate_state(state)
         state = np.expand_dims(state, axis=0)
@@ -163,3 +186,4 @@ class CriticNN(Critic, SplitGD):
         target = reward + self.discount_factor * self.model.predict(succ_state)
 
         self.fit(state, target)
+    """
